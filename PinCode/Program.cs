d09e5972f90982325e1e6affc0403f6c8a3e7b47
Rumana <rumanaparven16@gmail.com>
2020-10-05 09:07:42 +0530

    [Rumana] Add. Check for 6 digits in PIN Code

--- PinCode/Program.cs
+++ PinCode/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
+
 namespace PinCode
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the PIN Code : ");
+            String pin = Console.ReadLine();
+            RegexValidation rv = new RegexValidation();
+            rv.Validation(pin);
         }
     }
 }
---
+++ PinCode/RegexValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PinCode
+{
+    class RegexValidation
+    {
+        public void Validation(String pin)
+        {
+            Regex reg = new Regex("^[1-9]{1}[0-9]{5}$");
+            if (reg.IsMatch(pin))
+            {
+                Console.WriteLine("The PIN Code is Valid");
+            }
+            else
+            {
+                Console.WriteLine("The PIN Code is Invalid");
+            }
+        }
+    }
+}
